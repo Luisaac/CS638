e93b14f23f6a1eef6d4dc57f11e123ed09758835
Luisaac <luzhenda333@gmail.com>
2016-10-09 14:18:15 -0500

    Add files via upload

--- META-crawler.py
+++ META-crawler.py
@@ -27,6 +27,7 @@ def trade_spider(max_pages):
 					counter += 1
 					s = 'dir' + str(counter)
 					os.makedirs(s)
+				urllib.URLopener.version = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36 SE 2.X MetaSr 1.0'
 				urllib.urlretrieve(href, s +'/'+href.replace('/','@')+'.html')
 				number+=1
 		pageNum+=1
@@ -91,6 +92,6 @@ def get_single_item_data(item_url,f,num):
 		return 0
 	return 1
 
-trade_spider(22)
+trade_spider(14)
 
 
